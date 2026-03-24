84654fb0ea1dc6a79879b5e443b37f485ee43354
ivanovichi <ivan.arthur18@gmail.com>
2026-03-24 08:37:51 -0600

    feat: Use pretty dependency to make better syntax

--- src/practical-exp.jsx
+++ src/practical-exp.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
-import './general-info.css'
+import { useState } from "react";
+import "./general-info.css";
 
 function PracticalExperience() {
   return (
-        <fieldset id='section-3'>
-          <legend>Practical experience</legend>
-          <label htmlFor="company-name">Company name:</label>
-          <input type="text" id='company-name'/>
-          <label htmlFor="position-title">Position title:</label>
-          <input type="text" id='position-title'/>
-          <label htmlFor="main-responsibilities">Main responsibilities:</label>
-          <textarea id='main-responsibilities' rows={5} cols={1}></textarea>
-          <label htmlFor="date-start">Date when you started working:</label>
-          <input type="date" id='date-start'/>
-          <label htmlFor="date-finish">Date when you finished working:</label>
-          <input type="date" id='date-finish'/>
-        </fieldset>
-  )
+    <fieldset id="section-3">
+      <legend>Practical experience</legend>
+      <label htmlFor="company-name">Company name:</label>
+      <input type="text" id="company-name" />
+      <label htmlFor="position-title">Position title:</label>
+      <input type="text" id="position-title" />
+      <label htmlFor="main-responsibilities">Main responsibilities:</label>
+      <textarea id="main-responsibilities" rows={5} cols={1}></textarea>
+      <label htmlFor="date-start">Date when you started working:</label>
+      <input type="date" id="date-start" />
+      <label htmlFor="date-finish">Date when you finished working:</label>
+      <input type="date" id="date-finish" />
+    </fieldset>
+  );
 }
 
-export default PracticalExperience
+export default PracticalExperience;
